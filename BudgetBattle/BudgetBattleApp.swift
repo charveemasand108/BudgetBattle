6d6d6604b501bbde3159895ab00ec7df3e854483
Charvee Masand <charvee.masand@gmail.com>
2026-04-24 23:25:20 +0530

    Initial Commit

---
+++ BudgetBattle/BudgetBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBattleApp.swift
+//  BudgetBattle
+//
+//  Created by Charvee Masand on 24/04/26.
+//
+
+import SwiftUI
+
+@main
+struct BudgetBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
